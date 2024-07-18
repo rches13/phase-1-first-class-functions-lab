4adf6be5cc1f663fe1830aaf5c2db6c6f1c07e72
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-07-18 08:55:19 +0300

    add first class

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => (fare) => fare * multiplier;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, selectingFunction) => selectingFunction(drivers);
